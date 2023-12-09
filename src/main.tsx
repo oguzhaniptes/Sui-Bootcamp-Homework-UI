217befa2394648984c37fdcd0268d599d4f3eb50
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-10 01:03:03 +0300

    added wallet dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+import "./index.css";
+import "@mysten/dapp-kit/dist/index.css";
+import "@radix-ui/themes/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { getFullnodeUrl } from "@mysten/sui.js/client";
+
+import { SuiClientProvider, WalletProvider, createNetworkConfig } from "@mysten/dapp-kit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Router from "./Router/Router.tsx";
+
+const queryClient = new QueryClient();
+
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl("localnet") },
+  devnet: { url: getFullnodeUrl("devnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme appearance="dark">
+      <QueryClientProvider client={queryClient}>
+        <SuiClientProvider networks={networkConfig} defaultNetwork="devnet">
+          <WalletProvider autoConnect>
+            <Router />
+          </WalletProvider>
+        </SuiClientProvider>
+      </QueryClientProvider>
+    </Theme>
+  </React.StrictMode>
+);
