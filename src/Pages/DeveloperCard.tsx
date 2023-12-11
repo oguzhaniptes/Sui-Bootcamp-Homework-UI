f940a18c90320559ed72c64312666ba38afcbdfd
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-11 11:20:33 +0300

    devcard page added

--- src/Pages/DeveloperCard.tsx
+++ src/Pages/DeveloperCard.tsx
@@ -1,5 +1,27 @@
+import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
+import { Button } from "@radix-ui/themes";
+import { useMoveCalls } from "../Sui/DevHubCaller";
+// import { useWalletKit } from "@mysten/wallet-kit";
+
 const DeveloperCard = () => {
-  return <>aaaa</>;
+  const currentAccount = useCurrentAccount();
+  console.log(currentAccount);
+  const { handleCreateDeveloperCard } = useMoveCalls();
+
+  return (
+    <div>
+      <nav>
+        <ConnectButton></ConnectButton>
+      </nav>
+      <div style={{ marginTop: "2rem" }}>
+        {currentAccount && (
+          <Button size={"4"} onClick={handleCreateDeveloperCard}>
+            Create Developer Card
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DeveloperCard;
