33b9a33af0b84c832c874eee60a5a80b20f3aa2f
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-11 11:20:16 +0300

    devhub and contract id added

---
+++ src/Constant/Constant.tsx
@@ -0,0 +1,2 @@
+export const PACKAGE_OBJECT_ID = "0xa84c4c283b1353fd28628a7beed4f3f5fc3a03f8ed4b36b87b8e992d1ac80ca4";
+export const DEV_HUB = "0xc1bb1786f1ce297d56b8d6d303fb21a99942f908dbbcaf593342035bf9c0cad7";
