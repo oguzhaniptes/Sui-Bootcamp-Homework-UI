3ab12dbaae7d54a68919211e3828904928f36179
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-13 16:28:33 +0300

    created new pages

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import App from "../App";
 import DeveloperCard from "../Pages/DeveloperCard";
+import Integration from "../Pages/Integration";
+import GetCardInfo from "../Pages/GetCardInfo";
 
 const Router: React.FC = () => {
   return (
@@ -9,7 +11,9 @@ const Router: React.FC = () => {
       <BrowserRouter basename={"/"}>
         <Routes>
           <Route path="/" index element={<App />} />
-          <Route path="/integration" index element={<DeveloperCard />} />
+          <Route path="/integration" index element={<Integration />} />
+          <Route path="/create-developer-card" index element={<DeveloperCard />} />
+          <Route path="/get-card-info" index element={<GetCardInfo />} />
         </Routes>
       </BrowserRouter>
     </>
