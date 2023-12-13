0d84c5a8ab6e2b8a49766869b4290d54790dc674
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-13 21:25:20 +0300

    network constant added

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import { SuiClientProvider, WalletProvider, createNetworkConfig } from "@mysten/
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./Router/Router.tsx";
 import { WalletKitProvider } from "@mysten/wallet-kit";
+import { NETWORK } from "./Constant/Constant.tsx";
 
 const queryClient = new QueryClient();
 
@@ -25,7 +26,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Theme appearance="dark">
       <QueryClientProvider client={queryClient}>
-        <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <SuiClientProvider networks={networkConfig} defaultNetwork={NETWORK}>
           <WalletProvider autoConnect>
             <WalletKitProvider>
               <Router />
